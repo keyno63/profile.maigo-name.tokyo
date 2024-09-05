404197a79ff7a2388f39b9ebe5e49b7c0f01c5bb
keyno63 <kofujiw@gmail.com>
2024-09-06 04:29:10 +0900

    update profile

--- website/features/about/components/profile/profile.tsx
+++ website/features/about/components/profile/profile.tsx
@@ -4,6 +4,9 @@ export const Profile = () => {
     return (
         <div>
             <p className={styles.sample}>maigo-name profile</p>
+            <p className={styles.content}>メインはサーバーサイドエンジニア</p>
+            <p className={styles.content}>Webサイト、スマートフォンアプリの開発はプライベートでする程度</p>
+            <p className={styles.content}>データの分析、結果からの仮説検証のような作業に興味が強いです</p>
         </div>
     )
 }
