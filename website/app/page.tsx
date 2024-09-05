f03fc138e99b191be31b5e0d53e7a39926340e1d
keyno63 <kofujiw@gmail.com>
2024-09-06 04:28:43 +0900

    add header

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,9 +1,11 @@
 import { AboutPage } from "../features/about";
 import React from "react";
+import Header from "@/features/header";
 
 export default function Home() {
     return (
         <>
+            <Header />
             <AboutPage />
         </>
     );
---
+++ website/features/header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <h1 className={styles.title}>
+                maigo_name site
+            </h1>
+        </div>
+    )
+}
