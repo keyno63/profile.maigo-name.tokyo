48920be320360addd35a3e4936c9c3d7d944c0df
keyno63 <kofujiw@gmail.com>
2024-09-08 07:30:58 +0900

    add link icons

--- website/features/about/components/profile/profile.tsx
+++ website/features/about/components/profile/profile.tsx
@@ -8,6 +8,9 @@ export const Profile = () => {
                 <Icons/>
                 <div className={styles.profile_contents}>
                     <p className={styles.section_title}>Who am I?</p>
+                    <p className={styles.content_}>名前：</p>
+                    <p className={styles.content_}>ハンドルネーム: 名前迷子/keyno63</p>
+                    <p className={styles.content_}></p>
                     <p className={styles.content_}>職業プログラマーです</p>
                     <p className={styles.content_}>メインはサーバーサイドエンジニア</p>
                     <p className={styles.content_}>Webサイト、スマートフォンアプリの開発はプライベートでの経験</p>
