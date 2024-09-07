cc278ab1c92ea7f8eb4d6bc51c1f9faa614ba899
keyno63 <kofujiw@gmail.com>
2024-09-08 07:31:07 +0900

    update profile

--- website/features/about/components/profile/Icons.tsx
+++ website/features/about/components/profile/Icons.tsx
@@ -1,3 +1,7 @@
+import GitHubIcon from '@mui/icons-material/GitHub'
+import XIcon from '@mui/icons-material/X';
+import BookIcon from '@mui/icons-material/Book';
+
 export default function Icons() {
     return (
         <div>
@@ -8,6 +12,36 @@ export default function Icons() {
                     width={120}
                     height={120}
                 />
+                <a
+                    href="https://github.com/keyno63"
+                    target="_blank"
+                >
+                    <GitHubIcon fontSize="medium"/>
+                </a>
+                <a
+                    href="https://x.com/maigo_name"
+                    target="_blank"
+                >
+                    <XIcon fontSize="medium"/>
+                </a>
+                <a
+                    href="https://www.maigo-name.tokyo/"
+                    target="_blank"
+                >
+                    <BookIcon fontSize="medium"/>
+                </a>
+                <a
+                    href="https://zenn.dev/maigo_name"
+                    target="_blank"
+                >
+                    <BookIcon fontSize="medium"/>
+                </a>
+                <a
+                    href="https://lapras.com/public/0KCZQVD"
+                    target="_blank"
+                >
+                    <BookIcon fontSize="medium"/>
+                </a>
             </div>
         </div>
     );
