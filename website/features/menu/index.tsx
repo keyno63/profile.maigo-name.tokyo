0b4cd0820146c901baf3523ab958c6373d42411a
keyno63 <kofujiw@gmail.com>
2024-09-20 23:25:14 +0900

    add menu file

--- website/features/menu/index.tsx
+++ website/features/menu/index.tsx
@@ -1,4 +1,42 @@
+import Link from "next/link";
+
 type menuItem = {
     label: string
     href: string
+}
+
+const menus = [
+    {
+        label: "About",
+        href: "/",
+    },
+    {
+        label: "Blog",
+        href: "/blog",
+    },
+    {
+        label: "Contact",
+        href: "/contact",
+    },
+]
+
+export default function Menu() {
+
+    return <div className={undefined}>
+        <div className={undefined}>
+            {menus.map((menu) => {
+                return <MenuItem key={menu.label} {...menu}></MenuItem>
+            })}
+        </div>
+    </div>
+}
+
+function MenuItem(props: menuItem) {
+    return (
+        <div>
+            <Link href={props.href} as={props.href}>
+                {props.label}
+            </Link>
+        </div>
+    )
 }
