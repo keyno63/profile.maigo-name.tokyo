86c682d3a6ea9c2af0a11b829ca78868144da124
keyno63 <kofujiw@gmail.com>
2025-01-20 20:38:33 +0900

    update app server

--- backend/src/main/scala/tokyo/keyno63/profile/backend/AppServer.scala
+++ backend/src/main/scala/tokyo/keyno63/profile/backend/AppServer.scala
@@ -2,15 +2,13 @@ package tokyo.keyno63.profile.backend
 
 import zio._
 import zio.http._
-import zio.http.model._
+import zio.http.Method
 
 object AppServer extends ZIOAppDefault {
 
-    val app: HttpApp[Any, Nothing] = Http.collect[Request] {
-        case Method.GET -> !! / "hello" => Response.text("Hello, ZIO HTTP!")
-    }
+    private val routes = Routes(
+        Method.GET / "hello" -> handler(Response.text("Hello, World!"))
+    )
 
-    override def run: URIO[ZEnv, ExitCode] = {
-        Server.start(8080, app).exitCode
-    }
+    def run = Server.serve(routes).provide(Server.default)
 }
