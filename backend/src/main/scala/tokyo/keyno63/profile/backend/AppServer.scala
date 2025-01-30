c87d543e1c4e953f0c3e527b975c371f9c3b3bfb
keyno63 <kofujiw@gmail.com>
2025-01-30 23:41:20 +0900

    update AppServer

--- backend/src/main/scala/tokyo/keyno63/profile/backend/AppServer.scala
+++ backend/src/main/scala/tokyo/keyno63/profile/backend/AppServer.scala
@@ -7,6 +7,7 @@ import zio.http.Method
 object AppServer extends ZIOAppDefault {
 
     private val routes = Routes(
+        Method.GET / Root -> handler(Response.text("Root")),
         Method.GET / "hello" -> handler(Response.text("Hello, World!"))
     )
 
